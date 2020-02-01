fcf428c29e96d6352f8facb632f9118551571583
Grant Wise <42980443+granthwise@users.noreply.github.com>
2020-02-01 01:42:25 -0500

    Added Reading Table Data

--- app/src/main/java/com/example/worstcase/MapsActivity.java
+++ app/src/main/java/com/example/worstcase/MapsActivity.java
@@ -1,15 +1,33 @@
 package com.example.worstcase;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 //Proof of Concept Nate
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -42,6 +60,93 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+        //READING A FULL TABLE OF DATA
+
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAYO4MKXNF6QPMVBV3", "MiuoNvqvtGE9/xpnzQhIQGbjejGiWxD9xW3ECfYJ");
+        final AmazonDynamoDB ddb = new AmazonDynamoDBClient(awsCreds);
+        String table_name = "Blue-Light-Locations";
+        System.out.println("PAST CREDENTIALS");
+
+        final ScanRequest scanRequest1 = new ScanRequest()
+                .withTableName("Blue-Light-Locations")
+                .withAttributesToGet("Blue-Light-Number", "Location");
+        System.out.println("created the scan request");
+
+
+        class BasicallyAThread implements Runnable {
+            private volatile ScanResult result;
+
+            @Override
+            public void run() {
+                result = ddb.scan(scanRequest1);
+            }
+
+            public ScanResult getValue() {
+                return result;
+            }
+        }
+
+
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        BasicallyAThread foo = new BasicallyAThread();
+        Thread thread = new Thread(foo);
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ScanResult result = foo.getValue();
+
+        shelter[] shelterArray = new shelter[100];
+        int i = 0;
+        int counter = 0;
+        String number = "";
+        String longi = "";
+        String lat = "";
+        String food = "";
+        String water = "";
+        String medicine = "";
+        List<String> coord = new ArrayList<>();
+        List<String> supplies = new ArrayList<>();
+        boolean capacity = false;
+        String landmarks = "";
+
+        for (Map<String, AttributeValue> item : result.getItems()) {
+            Set<String> locations = item.keySet();
+            counter = 0;
+            for (String location : locations) {
+                if (counter == 0) {
+                    number = item.get(location).getN();
+                } else if (counter == 1) {
+                    coord = item.get(location).getNS();
+                }
+                else if(counter == 2){
+                    supplies = item.get(location).getNS();
+                }
+                else if(counter == 3){
+                    capacity = item.get(location).getBOOL();
+                }
+                else if(counter == 4){
+                    landmarks = item.get(location).getS();
+                }
+                counter++;
+            }
+            longi = coord.get(0);
+            lat = coord.get(1);
+            food = supplies.get(0);
+            water = supplies.get(1);
+            medicine = supplies.get(2);
+
+            shelterArray[i] = new shelter(Integer.valueOf(number), Double.valueOf(lat), Double.valueOf(longi),Integer.valueOf(food) ,Integer.valueOf(water) ,Integer.valueOf(medicine) ,Boolean.valueOf(capacity), String.valueOf(landmarks));
+            i++;
+        }
+
 
 
 
--- app/src/main/java/com/example/worstcase/shelter.java
+++ app/src/main/java/com/example/worstcase/shelter.java
@@ -3,7 +3,7 @@ package com.example.worstcase;
 //This was made
 
 public class shelter {
-
+    public int number;
     public double lat;
     public double longi;
     public int food;
@@ -12,7 +12,8 @@ public class shelter {
     public boolean capacity;
     public String name;
 
-    public shelter(double lat, double longi, int food, int water, int medicine, boolean capacity, String name) {
+    public shelter(int number, double lat, double longi, int food, int water, int medicine, boolean capacity, String name) {
+        this.number = number;
         this.lat = lat;
         this.longi = longi;
         this.food = food;
