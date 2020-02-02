376762cb0f920e7f60fdee307b5301b34a89a696
LAD-reposit <57956057+LAD-reposit@users.noreply.github.com>
2020-02-01 23:41:53 -0500

    buttons

---
+++ app/src/main/java/com/example/worstcase/intermediatePageButtons.java
@@ -0,0 +1,38 @@
+package com.example.worstcase;
+import android.widget.Button;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
+import java.lang.*;
+
+public class intermediatePageButtons extends android.app.Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intermediate_page);
+
+        Button newSheltr = (Button) findViewById(R.id.newShelt);
+
+        newSheltr.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                newShelter();
+            }
+        });
+
+        Button editr = (Button) findViewById(R.id.edit);
+
+        editr.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                editShelters();
+            }
+        });
+    }
+    public void newShelter() {
+        android.content.Intent intent= new android.content.Intent(this, OrganizerActivity.class);
+        startActivity(intent);
+    }
+    public void editShelters() {
+        android.content.Intent intent= new android.content.Intent(this, ShelterPuberty.class);
+        startActivity(intent);
+    }
+}
