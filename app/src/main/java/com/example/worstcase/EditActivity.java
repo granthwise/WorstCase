82eedab988dae3eee5d6740f81178b93ac5ce9b6
joegrows <52843618+joegrows@users.noreply.github.com>
2020-02-02 04:51:40 -0500

    grant

--- app/src/main/java/com/example/worstcase/EditActivity.java
+++ app/src/main/java/com/example/worstcase/EditActivity.java
@@ -21,25 +21,21 @@ public class EditActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                TextView organization_title = (TextView) findViewById(R.id.organization_title);
-                TextView organization_provisions_header = (TextView) findViewById(R.id.organization_provisions_header);
+                TextView update_title = (TextView) findViewById(R.id.update_title);
+                TextView edit_organization_provisions_header = (TextView) findViewById(R.id.edit_organization_provisions_header);
 
-                final CheckBox organization_checkbox_aid = (CheckBox) findViewById(R.id.organization_checkbox_aid);
-                final CheckBox organization_checkbox_food = (CheckBox) findViewById(R.id.organization_checkbox_food);
-                final CheckBox organization_checkbox_water = (CheckBox) findViewById(R.id.organization_checkbox_water);
-
-                EditText organization_shelter_latitude = (EditText) findViewById(R.id.organization_shelter_latitude);
-                EditText organization_shelter_longitude = (EditText) findViewById(R.id.organization_shelter_longitude);
-                EditText organization_shelter_id = (EditText) findViewById(R.id.organization_id);
-                EditText organization_shelter_maxcapacity = (EditText) findViewById(R.id.organization_shelter_maxcapacity);
-                EditText organization_shelter_landmarks = (EditText) findViewById(R.id.organization_shelter_landmarks);
+                EditText edit_organization_shelter_aid = (EditText) findViewById(R.id.edit_organization_shelter_aid);
+                EditText edit_organization_shelter_food = (EditText) findViewById(R.id.edit_organization_shelter_food);
+                EditText edit_organization_shelter_id = (EditText) findViewById(R.id.edit_organization_id);
+                EditText edit_organization_shelter_maxcapacity = (EditText) findViewById(R.id.edit_organization_shelter_maxcapacity);
+                EditText edit_organization_shelter_water = (EditText) findViewById(R.id.edit_organization_shelter_water);
 
                 //return these to dynamo
-                String organization_shelter_id_string = organization_shelter_id.getText().toString();
-                String organization_shelter_latitude_string = organization_shelter_latitude.getText().toString();
-                String organization_shelter_longitude_string = organization_shelter_longitude.getText().toString();
-                String organization_shelter_landmarks_string = organization_shelter_landmarks.getText().toString();
-                String organization_shelter_maxcapacity_string = organization_shelter_maxcapacity.getText().toString();
+                String organization_shelter_aid_string = edit_organization_shelter_aid.getText().toString();
+                String organization_shelter_food_string = edit_organization_shelter_food.getText().toString();
+                String organization_shelter_water_string = edit_organization_shelter_water.getText().toString();
+                String organization_shelter_id_string = edit_organization_shelter_id.getText().toString();
+                String organization_shelter_maxcapacity_string = edit_organization_shelter_maxcapacity.getText().toString();
 
                 final String[] organization_checkbox_aid_string = new String[1];
                 final String[] organization_checkbox_food_string = new String[1];
@@ -50,17 +46,8 @@ public class EditActivity extends AppCompatActivity {
                 organization_checkbox_water_string[0] = "false";
                 organization_checkbox_food_string[0] = "false";
 
-                if (organization_checkbox_aid.isChecked()) {
-                    organization_checkbox_aid_string[0] = "true";
-                }
 
-                if (organization_checkbox_food.isChecked()) {
-                    organization_checkbox_food_string[0] = "true";
-                }
-
-                if (organization_checkbox_water.isChecked()) {
-                    organization_checkbox_water_string[0] = "true";
-                }
+                //INFO GOES HERE GAIJASDINIDSSDNTY
 
             }
 
