3c238d787f9a5117916231dfb32434e6e8a6175e
Grant Wise <42980443+granthwise@users.noreply.github.com>
2020-02-02 05:04:34 -0500

    edit functionality

--- app/src/main/java/com/example/worstcase/EditActivity.java
+++ app/src/main/java/com/example/worstcase/EditActivity.java
@@ -9,6 +9,18 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.AttributeAction;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.AttributeValueUpdate;
+import com.amazonaws.services.dynamodbv2.model.UpdateItemRequest;
+import com.amazonaws.services.dynamodbv2.model.UpdateItemResult;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class EditActivity extends AppCompatActivity {
 
     @Override
@@ -46,8 +58,60 @@ public class EditActivity extends AppCompatActivity {
                 organization_checkbox_water_string[0] = "false";
                 organization_checkbox_food_string[0] = "false";
 
+                String maxCap = "";
 
-                //INFO GOES HERE GAIJASDINIDSSDNTY
+                if(organization_shelter_maxcapacity_string.equals("0")){
+                    maxCap = "false";
+                }
+                else{
+                    maxCap = "true";
+                }
+
+                BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAYO4MKXNF4PU4GKOT", "NvVpxh0NIAwZpk2zNjDL7ZDg+RMxQXnTK/Kpb4OX");
+                final AmazonDynamoDB ddb = new AmazonDynamoDBClient(awsCreds);
+
+
+
+                Map< String, AttributeValue> updatesuppliesMap = new HashMap<String,AttributeValue>();
+                updatesuppliesMap.put("Food",new AttributeValue().withS(organization_shelter_food_string));
+                updatesuppliesMap.put("Water",new AttributeValue().withS(organization_shelter_water_string));
+                updatesuppliesMap.put("Medicine",new AttributeValue().withS(organization_shelter_aid_string));
+
+
+
+
+                Map<String, AttributeValue> keysMap = new HashMap<>();
+                keysMap.put("ID", new AttributeValue().withN(organization_shelter_id_string));
+
+
+                final UpdateItemRequest updateRequest = new UpdateItemRequest()
+                        .withTableName("Shelters")
+                        .withKey(keysMap)
+                        .addAttributeUpdatesEntry("Supplies",new AttributeValueUpdate().withValue(new AttributeValue().withM(updatesuppliesMap)).withAction(AttributeAction.PUT))
+                        .addAttributeUpdatesEntry("Capacity",new AttributeValueUpdate().withValue(new AttributeValue().withS(maxCap)));
+
+
+                class BasicallyAUpdateThread implements Runnable {
+                    private volatile UpdateItemResult result;
+
+                    @Override
+                    public void run() {
+                        result = ddb.updateItem(updateRequest);
+                    }
+
+                    public UpdateItemResult getValue() {
+                        return result;
+                    }
+                }
+
+                BasicallyAUpdateThread yeet = new BasicallyAUpdateThread();
+                Thread thread4 = new Thread(yeet);
+                thread4.start();
+                try {
+                    thread4.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
             }
 
