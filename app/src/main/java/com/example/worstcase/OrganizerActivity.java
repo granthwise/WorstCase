c17cc537df5fd09cedbd4406e9b5d11a5df8d411
Grant Wise <42980443+granthwise@users.noreply.github.com>
2020-02-02 02:06:03 -0500

    integrated buttons and push database

--- app/src/main/java/com/example/worstcase/OrganizerActivity.java
+++ app/src/main/java/com/example/worstcase/OrganizerActivity.java
@@ -8,6 +8,16 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.dynamodbv2.model.PutItemResult;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class OrganizerActivity extends AppCompatActivity {
 
     @Override
@@ -35,12 +45,18 @@ public class OrganizerActivity extends AppCompatActivity {
         String organization_shelter_landmarks_string = organization_shelter_landmarks.getText().toString();
         String organization_shelter_maxcapacity_string = organization_shelter_maxcapacity.getText().toString();
 
+        final String[] organization_checkbox_aid_string = new String[1];
+        final String[] organization_checkbox_food_string = new String[1];
+        final String[] organization_checkbox_water_string = new String[1];
+
+
+
         organization_checkbox_aid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String organization_checkbox_aid_string = "false";
+                 organization_checkbox_aid_string[0] = "false";
                 if(organization_checkbox_aid.isChecked()){
-                    organization_checkbox_aid_string = "true";
+                    organization_checkbox_aid_string[0] = "true";
                 }
             }
         });
@@ -48,9 +64,9 @@ public class OrganizerActivity extends AppCompatActivity {
         organization_checkbox_food.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String organization_checkbox_food_string = "false";
+                organization_checkbox_food_string[0] = "false";
                 if(organization_checkbox_aid.isChecked()){
-                    organization_checkbox_food_string = "true";
+                    organization_checkbox_food_string[0] = "true";
                 }
             }
         });
@@ -58,15 +74,91 @@ public class OrganizerActivity extends AppCompatActivity {
         organization_checkbox_water.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String organization_checkbox_water_string = "false";
+                organization_checkbox_water_string[0] = "false";
                 if(organization_checkbox_water.isChecked()){
-                    organization_checkbox_water_string = "true";
+                    organization_checkbox_water_string[0] = "true";
                 }
             }
         });
 
-        ////////////////////// SEND THE STRINGS TO DYNAMO HERER/////////////////////////////////////
+        String food_string = "";
+        String med_string = "";
+        String water_string = "";
 
+
+        if(organization_checkbox_food_string[0].equals("false")){
+            food_string = "0";
+        }
+        else{
+            food_string = "2";
+        }
+
+        if(organization_checkbox_aid_string[0].equals("false")){
+            med_string = "0";
+        }
+        else{
+            med_string = "2";
+        }
+
+        if(organization_checkbox_water_string[0].equals("false")){
+            water_string = "0";
+        }
+        else{
+            water_string = "2";
+        }
+
+
+        ////////////////////// SEND THE STRINGS TO DYNAMO HERER/////////////////////////////////////
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAYO4MKXNF4PU4GKOT", "NvVpxh0NIAwZpk2zNjDL7ZDg+RMxQXnTK/Kpb4OX");
+        final AmazonDynamoDB ddb = new AmazonDynamoDBClient(awsCreds);
+
+
+        Map<String, AttributeValue> attributeValues = new HashMap<>();
+
+        attributeValues.put("ID",new AttributeValue().withN(organization_id_string));
+        Map< String,AttributeValue> coordMap = new HashMap<String,AttributeValue>();
+
+        coordMap.put("Lat",new AttributeValue().withS(organization_shelter_latitude_string));
+        coordMap.put("Long",new AttributeValue().withS(organization_shelter_longitude_string));
+        attributeValues.put("Coordinates",new AttributeValue().withM(coordMap));
+
+        Map< String,AttributeValue> suppliesMap = new HashMap<String,AttributeValue>();
+        suppliesMap.put("Food",new AttributeValue().withS(food_string));
+        suppliesMap.put("Water",new AttributeValue().withS(water_string));
+        suppliesMap.put("Medicine",new AttributeValue().withS(med_string));
+
+        attributeValues.put("Supplies",new AttributeValue().withM(suppliesMap));
+
+        attributeValues.put("Capacity",new AttributeValue().withS("false"));
+        attributeValues.put("Landmarks",new AttributeValue().withS(organization_shelter_landmarks_string));
+
+
+
+        final PutItemRequest request = new PutItemRequest()
+                .withTableName("Shelters")
+                .withItem(attributeValues);
+
+        class BasicallyAReadThread implements Runnable {
+            private volatile PutItemResult result;
+
+            @Override
+            public void run() {
+                result = ddb.putItem(request);
+            }
+
+            public PutItemResult getValue() {
+                return result;
+            }
+        }
+
+        BasicallyAReadThread bar = new BasicallyAReadThread();
+        Thread thread2 = new Thread(bar);
+        thread2.start();
+        try {
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }
