8555182f433f732be991b5951e1465d37ae2c8dd
joegrows <52843618+joegrows@users.noreply.github.com>
2020-02-01 23:38:54 -0500

    Organizer page up map working, no errors,
    not connected to dynamo

--- app/src/main/java/com/example/worstcase/shelter.java
+++ app/src/main/java/com/example/worstcase/shelter.java
@@ -1,9 +1,10 @@
+
 package com.example.worstcase;
 
 //This was made
 
 public class shelter {
-
+    public int number;
     public double lat;
     public double longi;
     public int food;
@@ -12,13 +13,19 @@ public class shelter {
     public boolean capacity;
     public String name;
 
-    public shelter(double lat, double longi, int food, int water, int medicine, boolean capacity, String name) {
+    public shelter(int number, double lat, double longi, int food, int water, int medicine, String tempCapacity, String name) {
+        this.number = number;
         this.lat = lat;
         this.longi = longi;
         this.food = food;
         this.water = water;
         this.medicine = medicine;
-        this.capacity = capacity;
+        if (tempCapacity.equals("true")) {
+            capacity = true;
+        }
+        else {
+            capacity = false;
+        }
         this.name = name;
     }
 
@@ -41,6 +48,44 @@ public class shelter {
     public int getMedicine() {
         return medicine;
     }
+
+    public String getStringFood() {
+        if (food == 0) {
+            return "None";
+        }
+        else if (food == 1) {
+            return "Low";
+        }
+        else {
+            return "Full";
+        }
+    }
+
+    public String getStringWater() {
+        if (water == 0) {
+            return "None";
+        }
+        else if (water == 1) {
+            return "Low";
+        }
+        else {
+            return "Full";
+        }
+    }
+
+    public String getStringMedicine() {
+        if (medicine == 0) {
+            return "None";
+        }
+        else if (medicine == 1) {
+            return "Low";
+        }
+        else {
+            return "Full";
+        }
+    }
+
+
 
     public boolean getCapacity() {
         return capacity;
