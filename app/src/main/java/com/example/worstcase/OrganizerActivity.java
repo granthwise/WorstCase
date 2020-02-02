8eb054d70501ead2707f8d00b462bfd2748aa782
joegrows <52843618+joegrows@users.noreply.github.com>
2020-02-02 00:52:32 -0500

    Organizer page mod
    added lat and long and id, no scroll

--- app/src/main/java/com/example/worstcase/OrganizerActivity.java
+++ app/src/main/java/com/example/worstcase/OrganizerActivity.java
@@ -19,21 +19,20 @@ public class OrganizerActivity extends AppCompatActivity {
         TextView organization_provisions_header = (TextView)findViewById(R.id.organization_provisions_header);
 
         final CheckBox organization_checkbox_aid = (CheckBox)findViewById(R.id.organization_checkbox_aid);
-        CheckBox organization_checkbox_food = (CheckBox)findViewById(R.id.organization_checkbox_food);
-        CheckBox organization_checkbox_water = (CheckBox)findViewById(R.id.organization_checkbox_water);
+        final CheckBox organization_checkbox_food = (CheckBox)findViewById(R.id.organization_checkbox_food);
+        final CheckBox organization_checkbox_water = (CheckBox)findViewById(R.id.organization_checkbox_water);
 
-        EditText organization_shelter_address = (EditText)findViewById(R.id.organization_shelter_address);
-        EditText organization_shelter_name = (EditText)findViewById(R.id.organization_name);
+        EditText organization_shelter_latitude = (EditText)findViewById(R.id.organization_shelter_latitude);
+        EditText organization_shelter_longitude = (EditText)findViewById(R.id.organization_shelter_longitude);
+        EditText organization_shelter_id = (EditText)findViewById(R.id.organization_id);
         EditText organization_shelter_maxcapacity = (EditText)findViewById(R.id.organization_shelter_maxcapacity);
         EditText organization_shelter_landmarks = (EditText)findViewById(R.id.organization_shelter_landmarks);
-
-        String organization_title_string = organization_title.getText().toString();
-        String organization_shelter_address_string = organization_shelter_address.getText().toString();
+        //return these to dynamo
+        String organization_id_string = organization_title.getText().toString();
+        String organization_shelter_latitude_string = organization_shelter_latitude.getText().toString();
+        String organization_shelter_longitude_string = organization_shelter_longitude.getText().toString();
         String organization_shelter_landmarks_string = organization_shelter_landmarks.getText().toString();
         String organization_shelter_maxcapacity_string = organization_shelter_maxcapacity.getText().toString();
-
-        String organization_checkbox_food_string = "false";
-        String organization_checkbox_water_string = "false";
 
         organization_checkbox_aid.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +40,26 @@ public class OrganizerActivity extends AppCompatActivity {
                 String organization_checkbox_aid_string = "false";
                 if(organization_checkbox_aid.isChecked()){
                     organization_checkbox_aid_string = "true";
+                }
+            }
+        });
+
+        organization_checkbox_food.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String organization_checkbox_food_string = "false";
+                if(organization_checkbox_aid.isChecked()){
+                    organization_checkbox_food_string = "true";
+                }
+            }
+        });
+
+        organization_checkbox_water.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String organization_checkbox_water_string = "false";
+                if(organization_checkbox_water.isChecked()){
+                    organization_checkbox_water_string = "true";
                 }
             }
         });
